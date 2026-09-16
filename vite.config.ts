5d82723b89569457c14ea3b91aac0d4eb46b1cb5
alpaslanbeyoglu <47739835+alpaslanbeyoglu@users.noreply.github.com>
2026-09-16 11:07:37 +0300

    build: update base path and script reference
    Adjust base path to /ALPAT/ and fix module script path to ensure correct routing in the production environment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/ALPAT/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
